6065ad48bc3a1c3c03919b99d8574a685f77a97a
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-03-02 21:48:54 +0900

    Title: 인접하지 않은 2개의 수, Time: 166ms, Memory: 10MB, Status: Passed - CodeTree

--- 250302/인접하지 않은 2개의 수/two-non-adjacent-numbers.java
+++ 250302/인접하지 않은 2개의 수/two-non-adjacent-numbers.java
@@ -12,7 +12,7 @@ public class Main {
 		int maxSum = 0;
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr.length; j++) {
-				if (Math.abs(i - j) > 2) {
+				if (Math.abs(i - j) >= 2) {
 					maxSum = Math.max(maxSum, (arr[i] + arr[j]));
 				}
 			}
