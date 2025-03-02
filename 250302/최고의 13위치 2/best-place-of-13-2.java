eafe64478b5e083e0d11460cf767c66e43ff8e8a
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-03-02 23:36:57 +0900

    Title: 최고의 13위치 2, Time: 160ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250302/최고의 13위치 2/best-place-of-13-2.java
+++ 250302/최고의 13위치 2/best-place-of-13-2.java
@@ -11,10 +11,8 @@ public class Main {
 		int maxCnt = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n - 2; j++) {
-				for (int k = 0; k < n; k++) {
+				for (int k = i + 1; k < n; k++) {
 					for (int l = 0; l < n - 2; l++) {
-						if (i == k && l >= j && l <= j + 2)
-							continue;
 						maxCnt = Math.max(maxCnt, arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[k][l] + arr[k][l + 1] + arr[k][l + 2]);
 					}
 				}
