97b3baedcadcb01b1f8dbfa94cb1591a4b1b1b4c
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-03-08 13:43:03 +0900

    Title: 삼각형 만들기, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250308/삼각형 만들기/create-triangle.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] x = new int[n];
+		int[] y = new int[n];
+		for (int i = 0; i < n; i++) {
+			x[i] = sc.nextInt();
+			y[i] = sc.nextInt();
+		}
+
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				for (int k = j + 1; k < n; k++) {
+					int minX = Math.min(Math.min(x[i], x[j]), x[k]);
+					int maxX = Math.max(Math.max(x[i], x[j]), x[k]);
+					int minY = Math.min(Math.min(y[i], y[j]), y[k]);
+					int maxY = Math.max(Math.max(y[i], y[j]), y[k]);
+
+					if ((x[i] == x[j] || x[j] == x[k] || x[k] == x[i]) && (y[i] == y[j] || y[j] == y[k] || y[k] == y[i]))
+						ans = Math.abs(maxX - minX) * Math.abs(maxY - minY);
+				}
+			}
+		}
+
+		System.out.println(ans);
+	}
+}
