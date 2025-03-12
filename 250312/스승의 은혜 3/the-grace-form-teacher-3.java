583e880e14af8db3abc51ec874108ba65049568f
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-03-12 14:46:20 +0900

    Title: 스승의 은혜 3, Time: 323ms, Memory: 16MB, Status: Passed - CodeTree

--- 250312/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 250312/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -1,23 +1,33 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int b = sc.nextInt();
-		int[] p = new int[n];
-		int[] s = new int[n];
+		int[][] p = new int[n][2];
 		for(int i = 0; i < n; i++){
-			p[i] = sc.nextInt();
-			s[i] = sc.nextInt();
+			p[i][0] = sc.nextInt();
+			p[i][1] = sc.nextInt();
 		}
+
+		int[][] sortedArr = Arrays
+				    .stream(p)
+				    .sorted(
+						    Comparator
+								    .comparingInt((int[] row) -> Arrays.stream(row).sum())
+								    .thenComparing((int[] row) -> row[0])
+				    )
+				    .toArray(int[][]::new);
 
 		int maxCnt = 0;
 		for (int i = 0; i < n; i++) {
 			int cnt = 0;
 			int tmpB = b;
 			for (int j = 0; j < n; j++) {
-				int price = p[j];
-				int delivery = s[j];
+				int price = sortedArr[j][0];
+				int delivery = sortedArr[j][1];
 
 				if (i == j)
 					price /= 2;
