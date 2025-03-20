3d344a7025e88b9313c9d517b6344bda847d7622
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-03-20 16:55:58 +0900

    Title: 3개의 선 2, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 250320/3개의 선 2/three-lines-2.java
+++ 250320/3개의 선 2/three-lines-2.java
@@ -11,31 +11,51 @@ public class Main {
 			y[i] = sc.nextInt();
 		}
 
-		for (int i = 0; i <= 21; i++) {
-			for (int j = 0; j <= 21; j++) {
-				for (int k = 0; k <= 21; k++) {
-					int[] arr = new int[n];
-
+		for (int i = 0; i <= 10; i++) {
+			for (int j = 0; j <= 10; j++) {
+				for (int k = 0; k <= 10; k++) {
+					boolean success = true;
 					for (int l = 0; l < n; l++) {
-						if (i < 11 && y[l] == i)
-							arr[l] = 1;
-						if (i >= 11 && x[l] == (i - 11))
-							arr[l] = 1;
-						if (j < 11 && y[l] == j)
-							arr[l] = 1;
-						if (j >= 11 && x[l] == (j - 11))
-							arr[l] = 1;
-						if (k < 11 && y[l] == k)
-							arr[l] = 1;
-						if (k >= 11 && x[l] == (k - 11))
-							arr[l] = 1;
+						if (y[l] == i || y[l] == j || y[l] == k)
+							continue;
+
+						success = false;
+					}
+					if (success) {
+						System.out.println(1);
+						return;
 					}
 
-					int sum = 0;
+					success = true;
 					for (int l = 0; l < n; l++) {
-						sum += arr[l];
+						if (y[l] == i || y[l] == j || x[l] == k)
+							continue;
+
+						success = false;
 					}
-					if (sum == n) {
+					if (success) {
+						System.out.println(1);
+						return;
+					}
+					success = true;
+					for (int l = 0; l < n; l++) {
+						if (y[l] == i || x[l] == j || x[l] == k)
+							continue;
+
+						success = false;
+					}
+					if (success) {
+						System.out.println(1);
+						return;
+					}
+					success = true;
+					for (int l = 0; l < n; l++) {
+						if (x[l] == i || x[l] == j || x[l] == k)
+							continue;
+
+						success = false;
+					}
+					if (success) {
 						System.out.println(1);
 						return;
 					}
