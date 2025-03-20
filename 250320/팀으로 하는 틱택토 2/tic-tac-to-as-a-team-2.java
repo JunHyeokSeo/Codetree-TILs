7c9d7ac36488516c2165cf8bcff57c96582adb9d
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-03-20 18:41:03 +0900

    Title: 팀으로 하는 틱택토 2, Time: 324ms, Memory: 16MB, Status: Passed - CodeTree

--- 250320/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
+++ 250320/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
@@ -30,16 +30,16 @@ public class Main {
 
 				boolean success = false;
 
-				if (tmp1.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2))) success = true;
-				if (tmp2.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2))) success = true;
-				if (tmp3.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2))) success = true;
-				if (tmp4.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2))) success = true;
-				if (tmp5.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2))) success = true;
-				if (tmp6.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2))) success = true;
-				if (tmp7.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2))) success = true;
-				if (tmp8.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2))) success = true;
+				if (tmp1.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2)) && tmp1.chars().anyMatch(c -> c == ('0' + num1)) && tmp1.chars().anyMatch(c -> c == ('0' + num2))) success = true;
+				if (tmp2.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2)) && tmp2.chars().anyMatch(c -> c == ('0' + num1)) && tmp2.chars().anyMatch(c -> c == ('0' + num2))) success = true;
+				if (tmp3.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2)) && tmp3.chars().anyMatch(c -> c == ('0' + num1)) && tmp3.chars().anyMatch(c -> c == ('0' + num2))) success = true;
+				if (tmp4.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2)) && tmp4.chars().anyMatch(c -> c == ('0' + num1)) && tmp4.chars().anyMatch(c -> c == ('0' + num2))) success = true;
+				if (tmp5.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2)) && tmp5.chars().anyMatch(c -> c == ('0' + num1)) && tmp5.chars().anyMatch(c -> c == ('0' + num2))) success = true;
+				if (tmp6.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2)) && tmp6.chars().anyMatch(c -> c == ('0' + num1)) && tmp6.chars().anyMatch(c -> c == ('0' + num2))) success = true;
+				if (tmp7.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2)) && tmp7.chars().anyMatch(c -> c == ('0' + num1)) && tmp7.chars().anyMatch(c -> c == ('0' + num2))) success = true;
+				if (tmp8.chars().allMatch(c -> c == ('0' + num1) || c == ('0' + num2)) && tmp8.chars().anyMatch(c -> c == ('0' + num1)) && tmp8.chars().anyMatch(c -> c == ('0' + num2))) success = true;
 
-				if (success){
+				if (success) {
 					cnt++;
 				}
 
