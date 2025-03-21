40a246da85ca0298c20387e15b68914adcaaa68c
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-03-21 19:09:07 +0900

    Title: 훌륭한 점프, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250321/훌륭한 점프/great-jump.java
+++ 250321/훌륭한 점프/great-jump.java
@@ -9,35 +9,24 @@ public class Main {
 			arr[i] = sc.nextInt();
 		}
 
-		int ans = 101;
-		for (int i = 100; i >= 1; i--) {
-			int cnt = 0;
-			int[] tmp = new int[n];
+		for (int i = Math.max(arr[0], arr[n - 1]); i <= 100; i++) {
+			int lastIdx = 0;
+			boolean success = true;
 			for (int j = 0; j < n; j++) {
-				if (arr[j] > i)
+				if (arr[j] < i)
 					continue;
-				tmp[cnt++] = j;
-			}
 
-			if (cnt <= 0)
-				continue;
-			
-			boolean success = (tmp[0] == 0 && tmp[cnt - 1] == n - 1);
-
-			for (int j = 1; j < cnt; j++) {
-				int diff = Math.abs(tmp[j] - tmp[j - 1]);
-				if (diff > k) {
+				if (j - lastIdx > k) {
 					success = false;
 					break;
 				}
+				lastIdx = j;
 			}
 
-			if (!success)
-				continue;
-
-			ans = Math.min(ans, i);
+			if (success) {
+				System.out.println(i);
+				break;
+			}
 		}
-
-		System.out.println(ans);
 	}
 }
