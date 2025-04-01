8bcc18a9962abc17f685aaa98812aea2e21183d3
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-04-01 12:31:05 +0900

    Title: 이상한 폭탄 3, Time: 115ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250401/이상한 폭탄 3/strange-bomb-3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int[] bombs = new int[n];
+		for (int i = 0; i < n; i++)
+			bombs[i] = sc.nextInt();
+
+		int maxCnt = 0;
+		int bomb = 0;
+		for (int i = 0; i < n; i++) {
+			int cnt = 0;
+			for (int j = i + 1; j < n; j++) {
+				if (bombs[i] == bombs[j] && Math.abs(i - j) <= k)
+					cnt++;
+			}
+
+			if (maxCnt < cnt)
+				bomb = bombs[i];
+			else if (maxCnt == cnt)
+				bomb = Math.max(bomb, bombs[i]);
+
+			maxCnt = Math.max(maxCnt, cnt);
+		}
+
+		System.out.println(bomb);
+	}
+}
