2e043bb548fb471b0835675fb6702ada8e48c4cc
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-04-02 15:37:27 +0900

    Title: 구간 잘 나누기, Time: 144ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250402/구간 잘 나누기/divide-sections-well.java
+++ 250402/구간 잘 나누기/divide-sections-well.java
@@ -1,70 +1,36 @@
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int m = sc.nextInt() - 1;
+		int m = sc.nextInt();
 		int[] nums = new int[n];
-				for (int i = 0; i < n; i++)
-					nums[i] = sc.nextInt();
+		for (int i = 0; i < n; i++)
+			nums[i] = sc.nextInt();
 
-		// 벽 초기화
-		int[] wall = new int[m];
+		for (int i = 1; i <= 10000; i++) {
+			boolean isPassable = true;
+			int section = 1;
+			int cnt = 0;
 
-		int minMax = Integer.MAX_VALUE;
-		while (wall[0] != n - 1) {
-			int pos = 0;
-			wall[m - 1]++;
-			for (int i = m - 1; i >= 0; i--) {
-				if (i != m - 1 && pos == 1)
-					wall[i]++;
-
-				pos = wall[i] / n;
-				wall[i] = wall[i] % n;
-			}
-			Set<Integer> set = Arrays.stream(wall).boxed().collect(Collectors.toSet());
-			if (wall.length != set.size())
-				continue;
-
-			boolean isAvailable = true;
-			for (int i = 0; i < m - 1; i++) {
-				if (wall[i] > wall[i + 1]) {
-					isAvailable = false;
+			for (int j = 0; j < n; j++) {
+				if (nums[j] > i) {
+					isPassable = false;
 					break;
 				}
-			}
 
-			if (!isAvailable)
-				continue;
-
-			if (wall[0] == 0) {
-				continue;
-			}
-
-			// 그룹별 최대값
-			int max = 0;
-			int last = 0;
-			for (int i = 0; i <= wall.length; i++) {
-				int sum = 0;
-				int end = i == wall.length ? n : wall[i];
-				for (int j = last; j < end; j++) {
-					sum += nums[j];
+				if (cnt + nums[j] > i) {
+					cnt = 0;
+					section++;
 				}
 
-				if (i != wall.length)
-					last = wall[i];
-
-				max = Math.max(max, sum);
+				cnt += nums[j];
 			}
 
-			minMax = Math.min(minMax, max);
+			if (isPassable && section == m) {
+				System.out.println(i);
+				return;
+			}
 		}
-
-		System.out.println(minMax);
 	}
 }
