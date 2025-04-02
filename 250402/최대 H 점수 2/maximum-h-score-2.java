1916e16ff430ef17677c75028959b8083c48cbbc
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-04-02 16:46:31 +0900

    Title: 최대 H 점수 2, Time: 118ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250402/최대 H 점수 2/maximum-h-score-2.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int l = sc.nextInt();
+		int[] a = new int[n];
+		for(int i = 0; i < n; i++)
+			a[i] = sc.nextInt();
+
+		int ans = 0;
+		for (int i = 1; i <= 100; i++) {
+			int[] tmp = Arrays.copyOf(a, a.length);
+			int cnt = 0;
+			for (int j = 0; j < n; j++) {
+				if (i - a[j] == 1 && cnt <= l) {
+					cnt++;
+					tmp[j] = i;
+				}
+			}
+
+			// H 탐색
+			for (int j = 0; j < n; j++) {
+				int hCnt = 0;
+				for (int k = 0; k < n; k++) {
+					if (j == k)
+						continue;
+
+					if (tmp[j] <= tmp[k])
+						hCnt++;
+				}
+				if (tmp[j] == hCnt)
+					ans = Math.max(ans, tmp[j]);
+			}
+		}
+
+		System.out.println(ans);
+		sc.close();
+	}
+}
