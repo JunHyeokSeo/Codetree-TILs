db45718470873013030e2acfa82d9e57605b4ed8
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-04-06 01:36:41 +0900

    Title: X 달리기, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250406/X 달리기/x-run.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+
+		int dist = 0;
+		int speed = 0;
+		int time = 0;
+		while (dist < x) {
+			int a = (speed * (speed + 1)) / 2;
+
+			if (dist + (speed + 1) + a <= x)
+				speed++;
+			else if (dist + a > x && speed > 1)
+				speed--;
+
+			dist += speed;
+
+			time++;
+		}
+
+		System.out.println(time);
+	}
+}
