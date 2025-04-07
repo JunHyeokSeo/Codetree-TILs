5ae98faca64330111b6c1cf388760bdaf7037652
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-04-07 14:10:47 +0900

    Title: 개발자의 가위바위보, Time: 144ms, Memory: 10MB, Status: Passed - Codetree

--- 250407/개발자의 가위바위보/developers-rock-paper-scissors.java
+++ 250407/개발자의 가위바위보/developers-rock-paper-scissors.java
@@ -15,6 +15,9 @@ public class Main {
 		for (int i = 1; i <= 3; i++) {
 			for (int j = 1; j <= 3; j++) {
 				for (int k = 1; k <= 3; k++) {
+                    if (i == j || j == k || k == i)
+                        continue;
+
 					int score = 0;
 					for (int l = 0; l < n; l++) {
 						if ((first[l] == i && second[l] == k) || (first[l] == j && second[l] == i) || (first[l] == k && second[l] == j))
