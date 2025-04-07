fc90a2356b0d0dfd2b706fd399362279ba9c2d19
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-04-07 12:11:31 +0900

    Title: 연속된 숫자 만들기 2, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 250407/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
+++ 250407/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -14,10 +14,10 @@ public class Main {
 			int ab = Math.abs(arr[0] - arr[1]);
 			int bc = Math.abs(arr[1] - arr[2]);
 
-			if (ab < bc && ab != 1) {
-				arr[2] = (arr[0] + arr[1]) / 2;
-			} else {
-				arr[0] = (arr[1] + arr[2]) / 2;
+			if ((ab <= bc && ab != 1) || (ab != 1 && bc == 1)) {
+				arr[2] = ab == 2 ? arr[0] + 1 : arr[0] + 2;
+			} else if (bc != 1) {
+				arr[0] = bc == 2 ? arr[1] + 1 : arr[1] + 2;
 			}
 
 			Arrays.sort(arr);
