28c87fc6bf11cfa94bdde6f223a5775831f0976c
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-04-07 12:51:29 +0900

    Title: 독서실의 거리두기 3, Time: 270ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250407/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		String seats = sc.next();
+
+		int[] arr = seats
+				            .chars()
+				            .map(c -> Integer.parseInt(String.valueOf((char) c)))
+				            .toArray();
+		int str = 0;
+		int end = 0;
+		int max = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == 1 && arr[j] == 1 && Math.abs(i - j) > max) {
+					max = Math.abs(i - j);
+					str = i;
+					end = j;
+					break;
+				}
+			}
+		}
+
+		arr[str + end / 2] = 1;
+
+		int minDiff = Integer.MAX_VALUE;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == 1 && arr[j] == 1) {
+					minDiff = Math.min(minDiff, Math.abs(i - j));
+					break;
+				}
+			}
+		}
+
+		System.out.println(minDiff);
+	}
+}
