94c32f5f1a937c69271a4fb2c4faae91dd1d9c15
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-04-12 14:21:23 +0900

    Title: 홀수 짝수의 묶음, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250412/홀수 짝수의 묶음/odd-even-bundle.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] numbers = new int[N];
+		int oddCnt = 0;
+		int evenCnt = 0;
+		int groupCnt = 0;
+		for (int i = 0; i < N; i++) {
+			numbers[i] = sc.nextInt();
+			if (numbers[i] % 2 == 0)
+				evenCnt++;
+			else
+				oddCnt++;
+		}
+
+		while (oddCnt > 0 || evenCnt > 0) {
+			if (groupCnt % 2 == 0) {
+				// 짝수
+				if (oddCnt >= 2) {
+					groupCnt++;
+					oddCnt -= 2;
+				} else if (evenCnt > 0) {
+					groupCnt++;
+					evenCnt--;
+				} else {
+					oddCnt--;
+					groupCnt--;
+				}
+			} else {
+				// 홀수
+				if (oddCnt > 0) {
+					oddCnt--;
+					groupCnt++;
+				} else {
+					evenCnt--;
+					groupCnt--;
+				}
+			}
+		}
+
+		System.out.println(groupCnt);
+	}
+}
