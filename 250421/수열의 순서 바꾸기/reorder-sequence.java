b6092124433340cc7049dff10af8355200c247c6
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-04-21 17:38:59 +0900

    Title: 수열의 순서 바꾸기, Time: 158ms, Memory: 10MB, Status: Passed - Codetree

--- 250421/수열의 순서 바꾸기/reorder-sequence.java
+++ 250421/수열의 순서 바꾸기/reorder-sequence.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -10,36 +9,11 @@ public class Main {
 			arr[i] = sc.nextInt();
 		}
 
-		int[] sortedArr = Arrays.stream(arr).sorted().toArray();
-
-		int cnt = 0;
-		while (!Arrays.equals(sortedArr, arr)) {
-			int firstItemInArr = arr[0];
-			int replaceIdx = 2;
-
-			while (replaceIdx < n) {
-				int tmp = replaceIdx - 1;
-
-				if (arr[replaceIdx] - arr[tmp] != 1) {
-					if (replaceIdx != n - 1) {
-						if (arr[replaceIdx + 1] - arr[replaceIdx] == 1)
-							replaceIdx--;
-					}
-
-					break;
-				}
-
-				replaceIdx++;
-			}
-
-			if (replaceIdx == n)
-				replaceIdx--;
-
-			System.arraycopy(arr, 1, arr, 0, replaceIdx);
-			arr[replaceIdx] = firstItemInArr;
-			cnt++;
+		int idx = n - 1;
+		while (idx > 0 && arr[idx - 1] < arr[idx]) {
+			idx--;
 		}
 
-		System.out.println(cnt);
+		System.out.println(idx);
 	}
 }
