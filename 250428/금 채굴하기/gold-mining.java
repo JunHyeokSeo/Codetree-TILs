a80250addcc85de9e9c4f0d57a8d1d896208f30d
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-04-28 16:52:59 +0900

    Title: 금 채굴하기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 250428/금 채굴하기/gold-mining.java
+++ 250428/금 채굴하기/gold-mining.java
@@ -11,7 +11,7 @@ public class Main {
 				grid[i][j] = sc.nextInt();
 
 		int ans = 0;
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i <= n; i++) {
 			int cost = (i * i) + ((i + 1) * (i + 1));
 			for (int j = 0; j < n; j++) {
 				for (int k = 0; k < n; k++) {
