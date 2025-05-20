aedfd16960ff2805daf1fe530826f7190edc0d3e
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-05-20 16:46:20 +0900

    Title: 대폭발, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250520/대폭발/big-explosion.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Main {
+	static int[] x = {1, -1, 0, 0};
+	static int[] y = {0, 0, -1, 1};
+	static int[][] grid;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int r = sc.nextInt() - 1;
+		int c = sc.nextInt() - 1;
+		grid = new int[n][n];
+		grid[r][c] = 1;
+		System.out.println("m = 0");
+		print();
+
+		for (int i = 1; i <= m; i++) {
+			int[][] tmp = new int[n][n];
+
+			for (int row = 0; row < n; row++) {
+				for (int col = 0; col < n; col++) {
+					if (grid[row][col] == 0)
+						continue;
+
+					tmp[row][col] = 1;
+					int dist = (int) Math.pow(2, (i - 1));
+
+					for (int j = 0; j < 4; j++) {
+						int tmpR = row + (y[j] * dist);
+						int tmpC = col + (x[j] * dist);
+
+						if (tmpR < 0 || tmpR > n - 1 || tmpC < 0 || tmpC > n - 1)
+							continue;
+
+						tmp[tmpR][tmpC] = 1;
+					}
+				}
+			}
+
+			grid = tmp;
+			System.out.println("m = " + i);
+			print();
+		}
+
+		int cnt = 0;
+		for (int[] ints : grid) {
+			for (int anInt : ints) {
+				cnt += anInt;
+			}
+		}
+
+		System.out.println(cnt);
+	}
+
+	public static void print() {
+		for (int[] ints : grid) {
+			for (int anInt : ints) {
+				System.out.print(anInt + " ");
+			}
+			System.out.println();
+		}
+	}
+}
