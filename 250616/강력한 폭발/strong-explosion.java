d29c7a0a38c62744d764a9835c0a1fbbaf91bd22
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-06-16 20:01:49 +0900

    Title: 강력한 폭발, Time: 141ms, Memory: 10MB, Status: Passed - Codetree

--- 250616/강력한 폭발/strong-explosion.java
+++ 250616/강력한 폭발/strong-explosion.java
@@ -1,23 +1,41 @@
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class Main {
+	public static class Point {
+		public int row;
+		public int col;
+
+		public Point(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
+	}
+
 	static int[] x = {0, 0, 0, 0, 0, 0, -1, 1, -1, 1, -1, 1};
 	static int[] y = {-2, -1, 1, 2, -1, 1, 0, 0, -1, -1, 1, 1};
 	static int maxExploreCnt = 0;
+	static List<Point> points = new ArrayList<>();
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[][] grid = new int[n][n];
-		int bombCnt = 0;
+		int bombCnt;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				int input = sc.nextInt();
 				grid[i][j] = input;
-				if (input == 1)
-					bombCnt++;
+
+				if (input == 1) {
+					points.add(new Point(i, j));
+				}
 			}
 		}
 
+		bombCnt = points.size();
 		getCnt(bombCnt, 0, n, grid);
 		System.out.println(maxExploreCnt);
 	}
@@ -37,17 +55,14 @@ public class Main {
 			return;
 		}
 
-		for (int row = 0; row < n; row++) {
-			for (int col = 0; col < n; col++) {
-				if (grid[row][col] != 1)
-					continue;
+		Point point = points.get(depth);
+		int row = point.row;
+		int col = point.col;
 
-				for (int bombKind = 0; bombKind < 3; bombKind++) {
-					bomb(row, col, n, bombKind, grid);
-					getCnt(bombCnt, depth + 1, n, grid);
-					recover(row, col, n, bombKind, grid);
-				}
-			}
+		for (int bombKind = 0; bombKind < 3; bombKind++) {
+			bomb(row, col, n, bombKind, grid);
+			getCnt(bombCnt, depth + 1, n, grid);
+			recover(row, col, n, bombKind, grid);
 		}
 	}
 
