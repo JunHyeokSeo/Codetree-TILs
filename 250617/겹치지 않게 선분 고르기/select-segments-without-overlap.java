efc2ecd4335ff627c956ff39116bf077ada63e49
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-06-17 11:14:11 +0900

    Title: 겹치지 않게 선분 고르기, Time: 138ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250617/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+	public static int n;
+	public static int[][] segments;
+	public static int cnt;
+	public static List<Integer> idx;
+	public static Set<Integer> idxForSearch;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		segments = new int[n][2];
+		cnt = 0;
+		idx = new ArrayList<>();
+		idxForSearch = new HashSet<>();
+		for (int i = 0; i < n; i++) {
+			segments[i][0] = sc.nextInt();
+			segments[i][1] = sc.nextInt();
+		}
+
+		getMaxLineCnt(0);
+		System.out.println(cnt);
+	}
+
+
+	public static void getMaxLineCnt(int depth) {
+		if (isOver() || depth == n) {
+			cnt = Math.max(cnt, depth);
+			return;
+		}
+
+		for (int i = 0; i < n; i++) {
+			//중복 검사
+			if (idxForSearch.contains(i))
+				continue;
+
+			idx.add(i);
+			idxForSearch.add(i);
+			getMaxLineCnt(depth + 1);
+			idx.remove(idx.size() - 1);
+			idxForSearch.remove(i);
+		}
+	}
+
+	public static boolean isOver() {
+		for (int i = 0; i < idx.size(); i++) {
+			for (int j = 0; j < idx.size(); j++) {
+				if (i == j)
+					continue;
+
+				if (!(segments[i][1] < segments[j][0] || segments[i][0] > segments[j][1]))
+					return true;
+			}
+		}
+
+		return false;
+	}
+}
