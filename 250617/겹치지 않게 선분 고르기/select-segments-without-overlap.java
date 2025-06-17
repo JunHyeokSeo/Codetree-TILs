c2a88fc90eb5904b0a6322e4677792ff29027f5f
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-06-17 20:06:58 +0900

    Title: 겹치지 않게 선분 고르기, Time: 3067ms, Memory: 48MB, Status: Time Limit Exceed - Codetree

--- 250617/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 250617/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -4,7 +4,8 @@ public class Main {
 	public static int n;
 	public static int[][] segments;
 	public static int cnt;
-	public static List<Integer> idx;
+	public static int[] idxArr;
+	public static int size;
 	public static Set<Integer> idxForSearch;
 
 	public static void main(String[] args) {
@@ -12,7 +13,9 @@ public class Main {
 		n = sc.nextInt();
 		segments = new int[n][2];
 		cnt = 0;
-		idx = new ArrayList<>();
+		idxArr = new int[n];
+		size = 0;
+
 		idxForSearch = new HashSet<>();
 		for (int i = 0; i < n; i++) {
 			segments[i][0] = sc.nextInt();
@@ -25,34 +28,30 @@ public class Main {
 
 
 	public static void getMaxLineCnt(int depth) {
-		boolean over = isOver();
+		boolean over = isOver(depth);
 		if (over || depth == n) {
 			cnt = Math.max(cnt, over ? depth - 1 : depth);
 			return;
 		}
 
 		for (int i = 0; i < n; i++) {
-			//중복 검사
 			if (idxForSearch.contains(i))
 				continue;
 
-			idx.add(i);
+			idxArr[depth] = i;
 			idxForSearch.add(i);
 			getMaxLineCnt(depth + 1);
-			idx.remove(idx.size() - 1);
 			idxForSearch.remove(i);
 		}
 	}
 
-	public static boolean isOver() {
-		for (int i = 0; i < idx.size(); i++) {
-			for (int j = 0; j < idx.size(); j++) {
-				if (i == j)
-					continue;
+	public static boolean isOver(int depth) {
+		if (depth <= 1)
+			return false;
 
-				if (!(segments[idx.get(i)][1] < segments[idx.get(j)][0] || segments[idx.get(i)][0] > segments[idx.get(j)][1]))
-					return true;
-			}
+		for (int i = 0; i < depth - 1; i++) {
+			if (!(segments[idxArr[depth - 1]][1] < segments[idxArr[i]][0] || segments[idxArr[depth - 1]][0] > segments[idxArr[i]][1]))
+				return true;
 		}
 
 		return false;
