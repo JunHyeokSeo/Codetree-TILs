71bdc8d68a9f03b17b4f38f6dabbb3876a0e113d
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-06-25 16:18:20 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 145ms, Memory: 10MB, Status: Passed - Codetree

--- 250625/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
+++ 250625/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Main {
 	public static int n;
-	public static boolean flag = true;
 	public static List<Integer> list;
 
 	public static void main(String[] args) {
@@ -17,42 +16,37 @@ public class Main {
 
 	public static void f(int index) {
 		if (index == n) {
-			if (flag) {
-				for (Integer i : list)
-					System.out.print(i);
-				flag = false;
-			}
-			return;
+			for (Integer i : list)
+				System.out.print(i);
+			System.exit(0);
 		}
 
 		for (int num = 4; num <= 6; num++) {
 			//진입 전에 가능한 수열인지 테스트
 			list.add(num);
-			if (flag && canNum())
+			if (canNum())
 				f(index + 1);
 			list.remove(list.size() - 1);
 		}
 	}
 
 	public static boolean canNum() {
-		boolean canNum = true;
-
 		for (int i = 1; i <= list.size() / 2; i++) {
-			boolean same = true;
 			int idxOriginStr = list.size() - i;
 			int idxCloseStr = idxOriginStr - i;
-			for (int j = 0; j < i; j++) {
-				if (!list.get(idxOriginStr + j).equals(list.get(idxCloseStr + j))) {
-					same = false;
-					break;
-				}
-			}
 
-			//같은게 하나라도 있으면
-			if (same)
-				canNum = false;
+			if (isEquals(idxOriginStr, idxCloseStr))
+				return false;
 		}
 
-		return canNum;
+		return true;
+	}
+
+	public static boolean isEquals(int idxOriginStr, int idxCloseStr) {
+		for (int i = 0; i < idxOriginStr - idxCloseStr; i++) {
+			if (!list.get(idxOriginStr + i).equals(list.get(idxCloseStr + i)))
+				return false;
+		}
+		return true;
 	}
 }
