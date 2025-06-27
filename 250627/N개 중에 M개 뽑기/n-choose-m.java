fe41f9880d5199799aa6fad6f18778cd494c7e73
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-06-27 20:59:34 +0900

    Title: N개 중에 M개 뽑기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250627/N개 중에 M개 뽑기/n-choose-m.java
+++ 250627/N개 중에 M개 뽑기/n-choose-m.java
@@ -1,33 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	static int n;
 	static int m;
-	static int[] arr;
+	static List<Integer> list;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		m = sc.nextInt();
-		arr = new int[m];
+		list = new ArrayList<>();
 
 		f(0, 0);
 	}
 
-	public static void f(int depth, int prevNum) {
-		if (depth == m) {
-			print();
+	public static void f(int depth, int cnt) {
+		if (depth == m + 1) {
+			if (cnt == m)
+				print();
 			return;
 		}
 
-		for (int i = prevNum + 1; i <= n; i++) {
-			arr[depth] = i;
-			f(depth + 1, i);
-		}
+		list.add(depth);
+		f(depth + 1, cnt + 1);
+		list.remove(list.size() - 1);
+
+		f(depth + 1, cnt);
 	}
 
 	public static void print() {
-		for (int i : arr)
+		for (Integer i : list) {
 			System.out.print(i + " ");
+		}
 		System.out.println();
 	}
 }
