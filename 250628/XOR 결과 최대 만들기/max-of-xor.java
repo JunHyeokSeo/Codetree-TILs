4d852a7cdb1b76faaf48f539e536e29b94a578d2
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-06-28 23:15:45 +0900

    Title: XOR 결과 최대 만들기, Time: 134ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250628/XOR 결과 최대 만들기/max-of-xor.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	static int n;
+	static int m;
+	static int ans = 0;
+	static int[] A;
+	static List<Integer> list = new ArrayList<>();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		A = new int[n];
+		for (int i = 0; i < n; i++)
+			A[i] = sc.nextInt();
+
+		f(1, 0);
+		System.out.println(ans);
+	}
+
+	public static void f(int depth, int cnt) {
+		if (depth > n) {
+			if (cnt == m)
+				ans = Math.max(ans, calcXOR());
+			return;
+		}
+
+		list.add(depth);
+		f(depth + 1, cnt + 1);
+		list.remove(list.size() - 1);
+		f(depth + 1, cnt);
+	}
+
+	public static int calcXOR() {
+		if (list.size() == 1)
+			return list.get(0);
+
+		int xor = list.get(0);
+		for (int i = 1; i < list.size(); i++) {
+			xor ^= list.get(i);
+		}
+		
+		return xor;
+	}
+}
