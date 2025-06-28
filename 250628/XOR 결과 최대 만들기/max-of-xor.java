bd1c25c0082d90c01875c70fd02c161130f2deff
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-06-28 23:18:41 +0900

    Title: XOR 결과 최대 만들기, Time: 293ms, Memory: 22MB, Status: Passed - Codetree

--- 250628/XOR 결과 최대 만들기/max-of-xor.java
+++ 250628/XOR 결과 최대 만들기/max-of-xor.java
@@ -16,21 +16,21 @@ public class Main {
 		for (int i = 0; i < n; i++)
 			A[i] = sc.nextInt();
 
-		f(1, 0);
+		f(0, 0);
 		System.out.println(ans);
 	}
 
-	public static void f(int depth, int cnt) {
-		if (depth > n) {
+	public static void f(int index, int cnt) {
+		if (index == n) {
 			if (cnt == m)
 				ans = Math.max(ans, calcXOR());
 			return;
 		}
 
-		list.add(depth);
-		f(depth + 1, cnt + 1);
+		list.add(A[index]);
+		f(index + 1, cnt + 1);
 		list.remove(list.size() - 1);
-		f(depth + 1, cnt);
+		f(index + 1, cnt);
 	}
 
 	public static int calcXOR() {
@@ -41,7 +41,7 @@ public class Main {
 		for (int i = 1; i < list.size(); i++) {
 			xor ^= list.get(i);
 		}
-		
+
 		return xor;
 	}
 }
