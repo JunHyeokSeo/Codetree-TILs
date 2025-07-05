8d8794510c86a6a006342f95ceeb697b459219a1
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-07-05 21:34:45 +0900

    Title: 외판원 순회, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250705/외판원 순회/traveling-salesman-problem.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static int n;
+	public static int ans = Integer.MAX_VALUE;
+	public static int[][] cost;
+	public static boolean[] visited;
+	public static List<Integer> per = new ArrayList<>();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		cost = new int[n][n];
+		visited = new boolean[n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cost[i][j] = sc.nextInt();
+			}
+		}
+
+		per.add(0);
+		f(1, 0);
+		System.out.println(ans);
+	}
+
+	public static void f(int index, int costSum) {
+		if (index == n) {
+			ans = Math.min(ans, costSum + cost[per.get(per.size() - 1)][0]);
+			return;
+		}
+
+		for (int i = 1; i < n; i++) {
+			if (visited[i])
+				continue;
+
+			int prevIdx = per.get(per.size() - 1);
+			visited[i] = true;
+			per.add(i);
+			f(index + 1, costSum + cost[prevIdx][i]);
+			per.remove(per.size() - 1);
+			visited[i] = false;
+		}
+	}
+}
