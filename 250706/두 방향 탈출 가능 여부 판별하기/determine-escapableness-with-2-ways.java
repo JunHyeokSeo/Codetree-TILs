6e7322b10091402391995c6e8060f6ebde23f943
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-07-06 15:35:50 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 301ms, Memory: 20MB, Status: Passed - Codetree

--- 250706/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
+++ 250706/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -3,6 +3,7 @@ public class Main {
 	public static int n;
 	public static int m;
 	public static int[][] grid;
+	public static int[][] visited;
 	public static int[] x = {0, 1};
 	public static int[] y = {1, 0};
 	public static void main(String[] args) {
@@ -10,36 +11,29 @@ public class Main {
 		n = sc.nextInt();
 		m = sc.nextInt();
 		grid = new int[n][m];
+		visited = new int[n][m];
 		for (int i = 0; i < n; i++)
 			for (int j = 0; j < m; j++)
 				grid[i][j] = sc.nextInt();
 
-
 		dfs(0, 0);
-		System.out.println(0);
+		System.out.println(visited[n - 1][m - 1]);
 	}
 
 	public static void dfs(int row, int col) {
-		if (row == n - 1 && col == m - 1) {
-			System.out.println(1);
-			System.exit(0);
-		}
-
 		for (int i = 0; i < 2; i++) {
 			int nextRow = row + y[i];
 			int nextCol = col + x[i];
 
 			if (canGo(nextRow, nextCol)) {
-				grid[row][col] = 0;
+				visited[nextRow][nextCol] = 1;
 				dfs(nextRow, nextCol);
 			}
 		}
 	}
 
 	public static boolean canGo(int row, int col) {
-		if (!inRange(row, col))
-			return false;
-		return grid[row][col] != 0;
+		return (inRange(row, col) && grid[row][col] == 1 && visited[row][col] == 0);
 	}
 
 	public static boolean inRange(int row, int col) {
