7785aea6c186e98824b42963cd21e5ee9219a3e2
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-07-06 14:38:07 +0900

    Title: 그래프 탐색, Time: 345ms, Memory: 24MB, Status: Passed - Codetree

--- 250706/그래프 탐색/graph-traversal.java
+++ 250706/그래프 탐색/graph-traversal.java
@@ -1,37 +1,42 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
 	public static int n;
 	public static int m;
 	public static int cnt = 0;
-	public static int[][] grid;
+	public static ArrayList<Integer>[] graph;
 	public static boolean[] visited;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		m = sc.nextInt();
-		grid = new int[n][n];
+		graph = new ArrayList[n];
+		for (int i = 0; i < n; i++) {
+			graph[i] = new ArrayList<>();
+		}
 		visited = new boolean[n];
 		for (int i = 0; i < m; i++) {
 			int v1 = sc.nextInt() - 1;
 			int v2 = sc.nextInt() - 1;
 
-			grid[v1][v2] = 1;
-			grid[v2][v1] = 1;
+			graph[v1].add(v2);
+			graph[v2].add(v1);
 		}
 
+		visited[0] = true;
 		dfs(0);
 		System.out.println(cnt);
 	}
 
 	public static void dfs(int vertex) {
-		for (int i = 1; i < n; i++) {
-			if (grid[vertex][i] != 1 || visited[i])
+		for (Integer node : graph[vertex]) {
+			if (visited[node])
 				continue;
 
-			visited[i] = true;
+			visited[node] = true;
 			cnt++;
-			dfs(i);
+			dfs(node);
 		}
 	}
 }
