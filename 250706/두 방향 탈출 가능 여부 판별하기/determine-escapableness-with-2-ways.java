55aed4587dc3e069c03218c75bd6dc1141ffc11f
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-07-06 14:54:15 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250706/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main {
+	public static int n;
+	public static int m;
+	public static int ans = 0;
+	public static int[][] grid;
+	public static int[] x = {0, 1};
+	public static int[] y = {1, 0};
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		grid = new int[n][m];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				grid[i][j] = sc.nextInt();
+
+		dfs(0, 0);
+		System.out.println(ans);
+	}
+
+	public static void dfs(int row, int col) {
+		for (int i = 0; i < 2; i++) {
+			int nextRow = row + y[i];
+			int nextCol = col + x[i];
+
+			if (canGo(nextRow, nextCol)) {
+				grid[nextRow][nextCol] = 0;
+				if (nextRow == n - 1 && nextCol == n - 1)
+					ans = 1;
+				dfs(nextRow, nextCol);
+			}
+		}
+	}
+
+	public static boolean canGo(int row, int col) {
+		if (!inRange(row, col))
+			return false;
+		return grid[row][col] != 0;
+	}
+
+	public static boolean inRange(int row, int col) {
+		return !(row < 0 || row > n - 1 || col < 0 || col > n - 1);
+	}
+}
