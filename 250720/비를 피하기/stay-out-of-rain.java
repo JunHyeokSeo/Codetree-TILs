b114a1b3df56c74551ed1bf98a2f06611275ee36
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-07-20 15:56:45 +0900

    Title: 비를 피하기, Time: 331ms, Memory: 45MB, Status: Passed - Codetree

--- 250720/비를 피하기/stay-out-of-rain.java
+++ 250720/비를 피하기/stay-out-of-rain.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Main {
 	static int n;
 	static int h;
@@ -12,18 +16,28 @@ public class Main {
 	static int[][] stepForPeople;
 	static boolean[][] visited;
 	static Queue<Pair> queue = new LinkedList<>();
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		h = sc.nextInt();
-		m = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// 첫 줄 입력 처리
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		// 배열 초기화
 		a = new int[n][n];
 		step = new int[n][n];
 		stepForPeople = new int[n][n];
 		visited = new boolean[n][n];
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
-				a[i][j] = sc.nextInt();
+
+		// 2차원 배열 입력 처리
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				a[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
 		findSaveArea();
 		print();
