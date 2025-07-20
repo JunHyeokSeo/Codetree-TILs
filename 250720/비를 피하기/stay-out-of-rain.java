13fb42204fc9ee3a8d56492ca8ef209bb6b92a1c
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-07-20 15:54:50 +0900

    Title: 비를 피하기, Time: 546ms, Memory: 49MB, Status: Passed - Codetree

---
+++ 250720/비를 피하기/stay-out-of-rain.java
@@ -0,0 +1,112 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+public class Main {
+	static int n;
+	static int h;
+	static int m;
+	static int[] x = {0, 0, -1, 1};
+	static int[] y = {-1, 1, 0, 0};
+	static int[][] a;
+	static int[][] step;
+	static int[][] stepForPeople;
+	static boolean[][] visited;
+	static Queue<Pair> queue = new LinkedList<>();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		h = sc.nextInt();
+		m = sc.nextInt();
+		a = new int[n][n];
+		step = new int[n][n];
+		stepForPeople = new int[n][n];
+		visited = new boolean[n][n];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				a[i][j] = sc.nextInt();
+
+		findSaveArea();
+		print();
+	}
+
+	public static void print() {
+		for (int[] stepForPerson : stepForPeople) {
+			for (int forPerson : stepForPerson) {
+				System.out.print(forPerson + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void init() {
+		step = new int[n][n];
+		visited = new boolean[n][n];
+	}
+
+	public static void findSaveArea() {
+		for (int row = 0; row < n; row++) {
+			for (int col = 0; col < n; col++) {
+				if (a[row][col] != 2)
+					continue;
+
+				init();
+				push(row, col, 0);
+				Pair pair = bfs();
+				if (pair == null) {
+					stepForPeople[row][col] = -1;
+				} else {
+					stepForPeople[row][col] = step[pair.row][pair.col];
+				}
+			}
+		}
+	}
+
+	public static Pair bfs() {
+		while (!queue.isEmpty()) {
+			Pair currV = queue.poll();
+
+			if (a[currV.row][currV.col] == 3) {
+				queue.clear();
+				return currV;
+			}
+
+			for (int i = 0; i < 4; i++) {
+				int nextRow = currV.row + y[i];
+				int nextCol = currV.col + x[i];
+
+				if (canGo(nextRow, nextCol)) {
+					push(nextRow, nextCol, step[currV.row][currV.col] + 1);
+				}
+			}
+		}
+
+		return null;
+	}
+
+	public static boolean canGo(int row, int col) {
+		if (!inRange(row, col))
+			return false;
+		if (a[row][col] == 1)
+			return false;
+		return !visited[row][col];
+	}
+
+	public static boolean inRange(int row, int col) {
+		return !(row < 0 || row > n - 1 || col < 0 || col > n - 1);
+	}
+
+	public static void push(int row, int col, int stepNum) {
+		queue.add(new Pair(row, col));
+		visited[row][col] = true;
+		step[row][col] = stepNum;
+	}
+
+	public static class Pair {
+		int row, col;
+
+		public Pair(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
+	}
+}
