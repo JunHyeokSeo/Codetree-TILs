e56cb85de8c5ab6ea59978f0cae914cfad020e68
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-07-25 16:02:19 +0900

    Title: 계단 오르기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250725/계단 오르기/climbing-stairs.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+	static int[] tab;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		if (n <= 3) {
+			System.out.println(1);
+			return;
+		}
+
+		tab = new int[n + 1];
+		tab[2] = 1;
+		tab[3] = 1;
+		dp(n);
+		System.out.println(tab[n] % 10007);
+	}
+
+	public static void dp(int n) {
+		for (int i = 4; i <= n; i++) {
+			tab[i] = tab[i - 2] + tab[i - 3];
+		}
+	}
+}
