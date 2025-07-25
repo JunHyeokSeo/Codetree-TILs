1b2e979ceac839afa14c2002e01c2f077cf108c7
JunHyeok Seo <55777781+JunHyeokSeo@users.noreply.github.com>
2025-07-25 15:50:07 +0900

    Title: 피보나치 수, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 250725/피보나치 수/fibonacci-number.java
+++ 250725/피보나치 수/fibonacci-number.java
@@ -5,6 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
+		if (n <= 2) {
+			System.out.println(1);
+			return;
+		}
+
 		memo = new int[n + 1];
 		memo[1] = 1;
 		memo[2] = 1;
